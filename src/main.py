6e8d7c21971b1c59c93bf906bd8085f7d276cc9f
heagge <henning.agge@gmail.com>
2026-01-20 20:12:04 +0100

    Master the Chungus with api there are 36 houres since my last real sleep

--- src/main.py
+++ src/main.py
@@ -1,2 +1,51 @@
+from constants import FilterHenningChannelId, FilterHenningUserId, Api_Key
+
+
 def main():
-    pass
+    playlists = getPlaylists("channel")
+    likedvideos = getLikedVideos("channel")
+    subscribedChannels = getSubscribedChannels("channel")
+    print("Playlists of Channel")
+    print(playlists)
+    print("liked videos of channel")
+    print(likedvideos)
+    print("subscribed Channels")
+    print(subscribedChannels)
+
+
+"""
+Youtube Api Waht do i Need to do?
+
+Playlists:
+GET https://www.googleapis.com/youtube/v3/playlists
+
+videos aus Playlists:
+
+
+Liked:
+
+Channels:
+
+Videos of channels:
+
+"""
+
+
+#Diese anfrage gibt die alle Playlists ids die ein Channel hat
+f"https://www.googleapis.com/youtube/v3/playlists?key={Api_Key}&part=contentDetails&channelId={FilterHenningChannelId}&maxResults=10"
+#dise anfrage gibt die die videos ids einer playlist du muss nurr die id bereit steleln
+f"https://www.googleapis.com/youtube/v3/playlistItems?key={Api_Key}&part=contentDetails&playlistId=PLg7eNtqimWhyPUjoBZoWK-5VcIkQfuQ4x&maxResults=5"
+
+
+
+
+
+
+
+#"id einer playlist"
+"PLg7eNtqimWhxzVBsWVm-rxpECNuUiEJ9w"
+
+#video id
+"fc0V8GHYiOw"
+#maybe ist das auch video id:¯\_(ツ)_/¯
+"UExnN2VOdHFpbVdoeVBVam9CWm9XSy01VmNJa1FmdVE0eC4yODlGNEE0NkRGMEEzMEQy"
