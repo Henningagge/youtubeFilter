ae7c713a45e7e1d48f4721c07ef9e035dc9b0664
heagge <henning.agge@gmail.com>
2026-01-21 19:19:14 +0100

    added youtube dependencies

--- src/Playlists.py
+++ src/Playlists.py
@@ -1,4 +1,11 @@
-def getPlaylistViaChannelId(id):
+import os
+from constants import currentTopicChannelId
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+def getPlaylistViaChannelId():
+    
 
 
 def insertIntoPlaylist(playlistid, videoid):
